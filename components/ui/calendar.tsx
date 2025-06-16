3972087479009cd11df112a6d6fd0ef96f082947
pranavgit135 <khandekarpranav52@gmail.com>
2025-06-17 00:01:27 +0530

    Update calendar.tsx

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
@@ -56,8 +55,8 @@ function Calendar({
 {/*       components={{
         IconLeft: () => <ChevronLeft className="h-4 w-4" />,
         IconRight: () => <ChevronRight className="h-4 w-4" />,
-      }} */}
-      {...props}
+      }}
+      {...props} */}
     />
   )
 }
